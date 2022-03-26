0c1787a5ea32559a3ea52d56c8cf51969571ecaf
H A L T <kamilrojek2001@gmail.com>
2022-03-26 15:59:50 +0100

    x

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -28,11 +28,17 @@ namespace Calculator
         }
         private void AddNo(object sender, RoutedEventArgs e)
         {
+            if(ResultText.Text!=string.Empty)
+            {
+                ResultText.Text=string.Empty;
+                Operation.Text=string.Empty;
+            }
             var button = sender as Button;
             var currentNumber = button.Name[button.Name.Length - 1];
+            ResultText.Text=string.Empty;
             if (!(IsHere(Operation.Text, ',')))
             {
-                if (ZChange(Operation.Text))
+                if (ZeroChange(Operation.Text))
                 {
                     var len = Operation.Text.Length;
                     Operation.Text = Operation.Text.Remove(len - 1, 1);
@@ -53,29 +59,75 @@ namespace Calculator
         }
         private void Add(object sender, RoutedEventArgs e)
         {
-            Operation.Text = calc(Operation.Text, '+');
-
+            if(!ResultText.Text.Contains('!'))
+            {
+                Operation.Text = calc(Operation.Text, '+');
+                Operation.Text=ResultCalc(Operation.Text,ResultText.Text,'+');
+                ResultText.Text=String.Empty;
+            }
         }
         private void Minus(object sender, RoutedEventArgs e)
         {
-            Operation.Text += '-';
+            if(!ResultText.Text.Contains('!'))
+            {
+            Operation.Text = calc(Operation.Text, '-');
+            Operation.Text=ResultCalc(Operation.Text,ResultText.Text,'-');
+            ResultText.Text=String.Empty;
+            }
         }
         private void Multiply(object sender, RoutedEventArgs e)
         {
-            Operation.Text += '*';
+            if(!ResultText.Text.Contains('!'))
+            {
+            Operation.Text = calc(Operation.Text, '*');
+            Operation.Text=ResultCalc(Operation.Text,ResultText.Text,'*');
+            ResultText.Text=String.Empty;
+            }
         }
         private void Divide(object sender, RoutedEventArgs e)
         {
-            Operation.Text += "/";
-        }
-        private void Result(object sender, RoutedEventArgs e)
-        {
-            ResultText.Text = Calculation(Operation.Text);
+            if(!ResultText.Text.Contains('!'))
+            {
+            Operation.Text = calc(Operation.Text, '/');
+            Operation.Text=ResultCalc(Operation.Text,ResultText.Text,'/');
+            ResultText.Text=String.Empty;
+            }
         }
         private void Coma(object sender, RoutedEventArgs e)
         {
-            Operation.Text += ',';
+            if(!ResultText.Text.Contains('!'))
+            {
+            Operation.Text = calc(Operation.Text, ',');
+            Operation.Text=Operation.Text +',';
+            ResultText.Text=String.Empty;
+            }
         }
+        private void Result(object sender, RoutedEventArgs e)
+        {
+            var len = Operation.Text.Length-1;
+            if(!(Operation.Text!=String.Empty&&ResultText.Text!=String.Empty))
+            {
+                if(!(Operation.Text[len]==('+')||Operation.Text[len]==('-')||Operation.Text[len]==('*')||Operation.Text[len]==('/')))
+                {
+                ResultText.Text = Calculation(Operation.Text);
+                }
+            }
+            else
+            {
+                var r = Operation.Text;
+                char p = 'x';
+                if(r.Contains('*')){p='*';}
+                else if(r.Contains('/')){p='/';}
+                else if(r.Contains('+')){p='+';}
+                else if(r.Contains('-')){p='-';}
+                var calc = r.Split(p);
+                r = ResultText.Text+p+calc[1];
+                Operation.Text=r;
+                ResultText.Text = Calculation(r);
+            }
+
+        }
+        
         private void AC(object sender, RoutedEventArgs e)
         {
             ResultText.Text = String.Empty;
@@ -134,23 +186,84 @@ namespace Calculator
         }
         private string calc(string operation, char x)
         {
+            if(operation.Length!=0)
+            {
             char[] tab = operation.ToCharArray();
             var i = tab.Length - 1;
-            if (tab[i] == '/' || tab[i] == '*' || tab[i] == '+' || tab[i] == '-')
+            if (tab[i] == '/' || tab[i] == '*' || tab[i] == '+' || tab[i] == '-' || tab[i]==',')
             {
                 tab[i] = x;
                 return new string(tab);
             }
             return new string(tab);
+            }
+            return operation;
+        }
+        private string ResultCalc(string operation,string result,char x) 
+        {
+            var ret="";
+            if(!(result!=string.Empty && operation!=string.Empty)) //jeżeli nic nie ma w konsoli, znak nie doda się
+            {
+                if(result==string.Empty) //jedynie znajduje się coś w operacji
+                {
+                    if(operation[0]=='-')
+                    {
+                        var operation2 = operation.Remove(0,1);
+                        var len = operation2.Length-1;
+                        if (operation2.Contains('+')||operation2.Contains('-')||operation2.Contains('*')||operation2.Contains('/'))
+                        {
+                            if(!(operation2[len]==('+')||operation2[len]==('-')||operation2[len]==('*')||operation2[len]==('/')))
+                            {
+                                ret = Calculation(operation2);
+                                ret+=x;
+                            }
+                            else
+                            {
+                                ret = operation;
+                            }
+                        }
+                        else
+                        {
+                            ret = operation+x;
+                        }
+                    }
+                    else
+                    {
+                        var len = operation.Length-1;
+                        if (operation.Contains('+')||operation.Contains('-')||operation.Contains('*')||operation.Contains('/'))
+                        {
+                           if(!(operation[len]==('+')||operation[len]==('-')||operation[len]==('*')||operation[len]==('/')))
+                            {
+                                ret = Calculation(operation);
+                                ret+=x;
+                            }
+                            else
+                            {
+                                ret = operation;
+                            }
+                        }
+                        else
+                        {
+                            ret = operation+x;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ret = result+x;
+            }
+            return ret;
+            
         }
 
-        private Boolean ZChange(string operation)
+        private Boolean ZeroChange(string operation)
         {
             var len = operation.Length;
             char[] tab = operation.ToCharArray();
             if (len != 0)
             {
-                if (tab[0] == '0')
+                if (tab[len-1] == '0')
                 { return true; }
 
                 else
